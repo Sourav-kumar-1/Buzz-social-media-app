53af15dead3e6ed73a6a74714be7aca07af627e5
SunishchitPandey <99393020+SunishchitPandey@users.noreply.github.com>
2022-04-20 03:05:42 +0530

    Update Suggestion.js

--- frontend/src/components/suggestion/Suggestion.js
+++ frontend/src/components/suggestion/Suggestion.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Userprofile from "../profile/userprofile/userprofile";
 /* import { SearchIcon } from "@primer/octicons-react"; */
 import Data from "./dummydata.json";
+import { useNavigate } from 'react-router-dom'
 import "./suggestion.css";
 function Suggestion({title}) {
   return (
+  
+    
+    
     <div className="suggestion-box">
       <nav className="navbar navbar-light bg-light">
         <div className="container-fluid">
@@ -22,15 +27,19 @@ function Suggestion({title}) {
 export default Suggestion;
 
 const SingleUser = ({data,title}) => {
+  
+
+  const navigate = useNavigate()
   return (
+
     <div className="username">
-      <img
+      <button className="bt" onClick={() => navigate("/userprofile", { replace: true })}><img
         className="image"
         src={require("./" + data.profilePicture)}
         alt="img not found"
       />{" "}
       {data.username}
-      {title === 'Suggestions' ? <><button className="bton">+ Friend</button></>: <></>}
+      {title === 'Suggestions' ? <><button className="bton" >+ Friend</button></>: <></>}</button>
     </div>
   );
 };
